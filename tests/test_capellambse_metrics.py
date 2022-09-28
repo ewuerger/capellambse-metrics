55acd94c94c93146166efc0517c872720f25da01
ewuerger <ernst.wuerger@gmail.com>
2022-09-28 10:08:03 +0200

    tests: Remove unused import

--- tests/test_capellambse_metrics.py
+++ tests/test_capellambse_metrics.py
@@ -1,8 +1,6 @@
 # SPDX-FileCopyrightText: Copyright capellambse-metrics contributors
 # SPDX-License-Identifier: Apache-2.0
 
-import capellambse_metrics
-
 
 def test_add_some_tests_here():
     ...
